83c71dbe966033ff0d98c809cc36a383891732d2
Rafael Barreto <rafaelbar@abcbrasil.com.br>
2022-12-17 18:48:14 -0300

    feature: Add handler to manipulate upload file

--- src/core/components/newGame/imageUpload.tsx
+++ src/core/components/newGame/imageUpload.tsx
@@ -1,17 +1,45 @@
-import { useRef } from "react";
+import { ChangeEvent, useRef } from "react";
 import SecundaryButton from "../shared/secundaryButton";
 
-export default function ImageUpload() {
+type changeProp = {
+  path: string;
+  value: Blob;
+}
+
+type ImageUploadProp = {
+  onChange?: (props: changeProp) => void;
+}
+
+export default function ImageUpload({ onChange }: ImageUploadProp) {
   const fileRef = useRef(null);
 
   const uploadClick = () => {
     fileRef.current.click();
   };
 
+  const onChangeInternal = (event: any) => {
+    const filePath = event.target["value"];
+
+    if (filePath) {
+      const fileBlob: Blob = event.target.files[0];
+
+      if (!fileBlob.type.match("image/")) {
+        //TODO add notification handler
+        console.error("invalid format");
+        return;
+      }
+
+      onChange?.({
+        path: filePath,
+        value: fileBlob
+      });
+    }
+  };
+
   return (
     <div>
       <SecundaryButton label="Selecione uma imagem" onClick={uploadClick} />
-      <input ref={fileRef} type="file" className="hidden" />
+      <input ref={fileRef} type="file" accept="image/*" className="hidden" onChange={onChangeInternal} />
     </div>
   );
 }
